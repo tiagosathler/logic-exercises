9dd6c782865d4287f07bcb78897e9e5cd03e92b1
Tiago <sathler@gmail.com>
2022-09-09 11:08:06 -0300

    :sparkles: feat: Implementation - conclui o desafio 'Granding'

---
+++ hackerrank/prepare/algorithms/implementation/granding.py
@@ -0,0 +1,51 @@
+#!/bin/python3
+
+# import math
+import os
+
+# import random
+# import re
+# import sys
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+
+def gradingStudents(grades):
+    """
+    https://www.hackerrank.com/challenges/grading/    
+    """
+    result = list()
+    for grade in grades:
+        if grade < 38:
+            result.append(grade)
+        elif grade % 5 == 0:
+            result.append(grade)
+        elif (((grade // 5) + 1) * 5) - grade < 3:
+            result.append((((grade // 5) + 1) * 5))
+        else:
+            result.append(grade)
+    return result
+
+
+if __name__ == "__main__":
+    fptr = open(os.environ["OUTPUT_PATH"], "w")
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write("\n".join(map(str, result)))
+    fptr.write("\n")
+
+    fptr.close()
