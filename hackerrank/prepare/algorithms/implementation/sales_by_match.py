000951f573ca6d257c806c2447c41132aca2a4ab
Tiago <sathler@gmail.com>
2022-09-13 20:44:40 -0300

    :sparkles: feat: Implementation - conclui o desafio 'Sales By Match'

---
+++ hackerrank/prepare/algorithms/implementation/sales_by_match.py
@@ -0,0 +1,45 @@
+#!/bin/python3
+
+# import math
+import os
+# import random
+# import re
+# import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+
+def sockMerchant(n: int, ar: list[int]) -> int:
+    """
+    https://www.hackerrank.com/challenges/sock-merchant/
+    """
+    socks = dict.fromkeys(ar, 0)
+    for color in ar:
+        socks[color] += 1
+
+    pairs = 0
+    for quantities in socks.values():
+        pairs += quantities // 2
+
+    return pairs
+
+
+if __name__ == "__main__":
+    fptr = open(os.environ["OUTPUT_PATH"], "w")
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + "\n")
+
+    fptr.close()
