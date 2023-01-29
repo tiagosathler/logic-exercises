bcf7d92b5e5fee352591f82af1fbceb85c563bb6
Tiago Heringer Siqueira Sathler <sathler@gmail.com>
2023-01-28 23:16:14 -0300

    :test_tube: test: add test for challenge 09 - smallest_multiple

---
+++ hackerrank/trybe/tests/test_09.py
@@ -0,0 +1,19 @@
+from src.c09_smallest_multiple import smallest_multiple
+import timeit
+
+
+def test_smallest_multiple() -> None:
+    setup_import = "from src.c09_smallest_multiple import smallest_multiple"
+    n = 20
+
+    correct_response = smallest_multiple(n) == 232792560
+
+    time = timeit.timeit(
+        "smallest_multiple(20)", setup=f"{setup_import}", number=1,
+    )
+
+    correct_time = time <= 0.005
+
+    assert (
+        correct_response and correct_time
+    ), f"Falhou, o tempo foi: {time}, algoritmo correto? {correct_response}"
