3c73fe168751a08dad4ba782aeaba67c5164ed9f
Tiago Heringer Siqueira Sathler <sathler@gmail.com>
2023-01-29 22:17:27 -0300

    :bulb: docs: challenge 08 - standardize the comments

--- hackerrank/trybe/src/c08_caixa.py
+++ hackerrank/trybe/src/c08_caixa.py
@@ -1,22 +1,43 @@
-"""Challenge 08 - Supermarket cashier"""
+"""
+Challenge 08 - Caixa de supermercado - supermarket_cashier
+
+Uma atendente de supermercado deve lançar os valores
+dos produtos à medida que os passa no caixa.
+
+Eventualmente ele faz um lançamento errado e para
+invalidá-lo deve lançar o valor 0 (zero) para que
+o registro anterior não seja considerado.
+
+Na eventualidade de mais de um valor ser lançado errado
+consecutivamente o atendente lançará tantos valores zero
+quanto necessários para apagar os registros incorretos.
+
+Por exemplo, no lançamento: 1, 3, 5, 4, 0, 0, 7, 0, 0, 6
+serão considerados para a soma os valores 1 e 6,
+visto que os valores 3, 5, 4 e 7 foram anulados pelos zeros.
+
+Construa um algoritmo capaz de processar uma entrada de
+n números e apresentar na saída a SOMA DOS REGISTROS VÁLIDOS.
+
+Considere a entrada do algoritmo sendo uma lista da seguinte
+forma: [n1, n2, n3, n4, n5, ... nx].
+
+No exemplo acima a entrada seria [1, 3, 5, 4, 0, 0, 7, 0, 0, 6]
+
+* Caso você analise os dados do teste aberto verá que a entrada é:
+10, 1, 3, 5, 4, 0, 0, 7, 0, 0, 6. Mas apenas a lista
+[1, 3, 5, 4, 0, 0, 7, 0, 0, 6] é passada para a função que você
+irá construir.
+"""
+
 from collections import deque
 from typing import List
 
 
 def caixa(arg: int, seq: List[int]) -> int:
-    """Uma atendente de supermercado deve lançar os valores
-    dos produtos à medida que os passa no caixa.
-    Eventualmente ele faz um lançamento errado e para
-    invalidá-lo deve lançar o valor 0 (zero) para que
-    o registro anterior não seja considerado.
-    Na eventualidade de mais de um valor ser lançado errado
-    consecutivamente o atendente lançará tantos valores zero
-    quanto necessários para apagar os registros incorretos.
-    Por exemplo, no lançamento: 1, 3, 5, 4, 0, 0, 7, 0, 0, 6
-    serão considerados para a soma os valores 1 e 6,
-     visto que os valores 3, 5, 4 e 7 foram anulados pelos zeros.
-    Construa um algoritmo capaz de processar uma entrada de
-    n números e apresentar na saída a SOMA DOS REGISTROS VÁLIDOS.
+    """
+    Calcula a soma das 'arg' operações da lista 'seq'
+    de uma lógica de um caixa de supermercado.
 
     Args:
         arg (int): any positive integer - number of operations
