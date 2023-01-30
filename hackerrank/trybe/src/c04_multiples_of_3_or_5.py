af2f46fb73c87e927ad436aafbebf1d73d4db49f
Tiago Heringer Siqueira Sathler <sathler@gmail.com>
2023-01-29 21:43:17 -0300

    :bulb: docs: challenge 04 - standardize the comments

--- hackerrank/trybe/src/c04_multiples_of_3_or_5.py
+++ hackerrank/trybe/src/c04_multiples_of_3_or_5.py
@@ -1,9 +1,19 @@
-"""Challenge 04 - Multiples of 3 or 5"""
+"""
+Challenge 04 - Múltiplos de 3 ou 5 - multiples_of_3_or_5
+
+Construa um algoritmo que encontre a soma de todos os números
+naturais múltiplos de 3 ou 5 menores que um dado número.
+
+Exemplo:
+Quando listamos todos os números naturais menores que 10
+que são múltiplos de 3 ou 5 temos: 3, 5, 6 e 9.
+A soma desses números é 3 + 5 + 6 + 9 = 23
+"""
 
 
 def multiples_of_3_or_5(arg: int) -> int:
-    """Construa um algoritmo que encontre a soma de todos os números naturais
-    múltiplos de 3 ou 5 menores que um dado número 'x'.
+    """Calcula a soma de todos os números naturais
+    múltiplos de 3 ou 5 menores que um dado número 'arg'.
 
     Args:
         arg (int): any positive integer
