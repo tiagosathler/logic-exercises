d165dafa1c2f3f84c6e5dc555ccbec9480d85732
Tiago Heringer Siqueira Sathler <sathler@gmail.com>
2023-01-29 21:36:36 -0300

    :bulb: docs: challenge 02 - standardize the comments

--- hackerrank/trybe/src/c02_difference.py
+++ hackerrank/trybe/src/c02_difference.py
@@ -1,4 +1,13 @@
-"""Challenge 02 - Difference"""
+"""
+Challenge 02 - Diferença - difference
+
+Dado um número X, retorne a diferença entre X e 10
+de forma que o resultado seja sempre um valor positivo.
+
+Isto é:
+* se X < 10, faça 10 - X, ex: X = 3 -> 7 (10 - 3)
+* se X > 10, faça X - 10, ex: X = 15 -> 5 (15 - 10)
+"""
 
 
 def difference(arg: int) -> int:
