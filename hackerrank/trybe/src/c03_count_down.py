6fab27338ad037a96d0d8103bb90723e1f305174
Tiago Heringer Siqueira Sathler <sathler@gmail.com>
2023-01-29 21:39:56 -0300

    :bulb: docs: challenge 03 - standardize the comments

--- hackerrank/trybe/src/c03_count_down.py
+++ hackerrank/trybe/src/c03_count_down.py
@@ -1,9 +1,16 @@
-"""Challenge 03 - Count down"""
+"""
+Challenge 03 - Contagem regressiva - count_down
+
+Dado um número X construa um programa que retorne
+a contagem regressiva no formato X...X-1...X-2...0!!!
+
+Exemplo:
+* X = 10 -> 10...9...8...7...6...5...4...3...2...1...0!!!
+"""
 
 
 def count_down(arg: int) -> str:
-    """Dado um número 'x', construa um programa que retorne a contagem
-    regressiva no formato "x...x-1...x-2...0!!!
+    """Retorna a contagem regressiva no formato "x...x-1...x-2...0!!!
 
     Args:
         arg (int): any positive integer
