92d5d3499afdcf35603137ac2cea73a7571ba9f2
Tiago Heringer Siqueira Sathler <sathler@gmail.com>
2023-01-29 21:33:04 -0300

    :bulb: docs: challenge 01 - standardize the comments

--- hackerrank/trybe/src/c01_conversor_fahrenheit_celsius.py
+++ hackerrank/trybe/src/c01_conversor_fahrenheit_celsius.py
@@ -1,4 +1,18 @@
-"""Challenge 01 - Conversor Fahrenheit to Celsius temperature"""
+"""
+Challenge 01 - Conversor Fahrenheit-Celsius - fahrenheit_to_celsius
+
+Para se converter uma temperatura medida em um termômetro em Fahrenheit
+para escala Celsius, com a qual estamos acostumados no Brasil deve-se
+usar a equação:
+* C = (F - 32)/1,8, onde C é a temperatura em graus Celsius e F a
+temperatura medida em Fahrenheit.
+
+Construa um algoritmo que, ao receber um valor de temperatura medido
+em Fahrenheit retorne a medida equivalente em Celsius.
+
+Por exemplo, ao receber a leitura de 95ºF retorne 35,0
+- Atenção para a saída em número de ponto flutuante.
+"""
 
 
 def fahrenheit_to_celsius(arg: float) -> float:
