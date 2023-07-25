0236540164653baae8ad158599a420067d9649c3
Tiago Heringer Siqueira Sathler <sathler@gmail.com>
2023-07-25 15:52:23 -0300

    :recycle: refactor: refatora o desafio 'Bill Division'

--- hackerrank/prepare/algorithms/implementation/bill_division.py
+++ hackerrank/prepare/algorithms/implementation/bill_division.py
@@ -1,4 +1,7 @@
-#!/bin/python3
+"""
+Bill Division
+https://www.hackerrank.com/challenges/bon-appetit/
+"""
 
 # import math
 # import os
@@ -16,15 +19,18 @@
 #
 
 
-def bonAppetit(bill: list[int], k: int, b: int) -> None:
+def bon_appetit(bill: list[int], do_not_eat: int, money: int) -> None:
+    """Bon Appetit function
+
+    Args:
+        bill (list[int]): an array of integers representing
+        the cost of each item ordered;
+        do_not_eat (int): an integer representing the zero-based
+        index of the item Anna doesn't eat;
+        money (int): the amount of money that Anna contributed to the bill.
     """
-    https://www.hackerrank.com/challenges/bon-appetit/
-    """
-    sum = 0
-    for expense in bill:
-        sum += expense
-    actual = (sum - bill[k]) / 2
-    diff = b - actual
+    total = (sum(bill) - bill[do_not_eat]) / 2
+    diff = money - total
 
     if diff == 0:
         print("Bon Appetit")
@@ -33,14 +39,14 @@ def bonAppetit(bill: list[int], k: int, b: int) -> None:
 
 
 if __name__ == "__main__":
-    first_multiple_input = input().rstrip().split()
+    # first_multiple_input = input().rstrip().split()
 
-    n = int(first_multiple_input[0])
+    # n = int(first_multiple_input[0])
 
-    k = int(first_multiple_input[1])
+    # k = int(first_multiple_input[1])
 
-    bill = list(map(int, input().rstrip().split()))
+    # bill = list(map(int, input().rstrip().split()))
 
-    b = int(input().strip())
+    # b = int(input().strip())
 
-    bonAppetit(bill, k, b)
+    bon_appetit([3, 10, 2, 9], 1, 12)
