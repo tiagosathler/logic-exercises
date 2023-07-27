f5f28980218f367d0bf4fb1db7a30c53479c5e62
Tiago Heringer Siqueira Sathler <sathler@gmail.com>
2023-07-27 17:21:59 -0300

    :recycle: refactor: refatora o desafio 'Couting Valleys'

--- hackerrank/prepare/algorithms/implementation/couting_valleys.py
+++ hackerrank/prepare/algorithms/implementation/couting_valleys.py
@@ -1,7 +1,10 @@
-#!/bin/python3
+"""HackerRank
+Counting Valleys
+https://www.hackerrank.com/challenges/counting-valleys
+"""
+
 
 # import math
-
 # import random
 # import re
 # import sys
@@ -13,36 +16,44 @@
 # The function accepts following parameters:
 #  1. INTEGER steps
 #  2. STRING path
-#
 
 
-def countingValleys(steps: int, path: str) -> int:
-    """
-    https://www.hackerrank.com/challenges/counting-valleys
+def counting_valleys(steps: int, path: str) -> int:
+    """Counting the number of valleys
+
+    Args:
+        steps (int): the number of steps on the hike
+        path (str): a string describing the path
+
+    Returns:
+        int: the number of valleys traversed
     """
     valleys = 0
     level = 0
-    for step in path:
-        if step == "U":
+
+    for step in range(0, steps):
+        if path[step] == "U":
             level += 1
         else:
             level -= 1
-        if level == 0 and step == "U":
+
+        if level == 0 and path[step] == "U":
             valleys += 1
+
     return valleys
 
 
 if __name__ == "__main__":
-    # fptr = open(os.environ["OUTPUT_PATH"], "w")
+    # # fptr = open(os.environ["OUTPUT_PATH"], "w")
 
-    # steps = int(input().strip())
-    steps = 8
+    # # steps = int(input().strip())
+    # steps = 8
 
-    # path = input()
-    path = 'UDDDUDUU'
+    # # path = input()
+    # path = 'UDDDUDUU'
 
-    result = countingValleys(steps, path)
+    # result = countingValleys(steps, path)
 
     # fptr.write(str(result) + "\n")
-    print(result)
     # fptr.close()
+    print(counting_valleys(8, "UDDDUDUU"))
